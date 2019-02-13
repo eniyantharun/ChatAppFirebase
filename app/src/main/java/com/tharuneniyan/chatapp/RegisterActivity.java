01162bfa86c086f6aa5404a6038e6ca7094fbffb
tharun <tharuneniyan4@gmail.com>
2019-02-13 12:55:35 +0530

    Removed Textinputlayout error

--- app/src/main/java/com/tharuneniyan/chatapp/RegisterActivity.java
+++ app/src/main/java/com/tharuneniyan/chatapp/RegisterActivity.java
@@ -1,24 +1,24 @@
 package com.tharuneniyan.chatapp;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
-    private TextInputLayout mDisplayName;
-    private TextInputLayout mEmail;
-    private TextInputLayout mPassword;
+    private EditText mDisplayName;
+    private EditText mEmail;
+    private EditText mPassword;
     private Button mCreateBtn;
     private String TAG = RegisterActivity.class.getSimpleName();
     private FirebaseAuth mAuth;
@@ -37,38 +37,28 @@ public class RegisterActivity extends AppCompatActivity {
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String display_Name = mDisplayName.getEditText().getText().toString();
-                String email = mEmail.getEditText().getText().toString();
-                String password= mPassword.getEditText().getText().toString();
+                String display_Name = mDisplayName.getText().toString();
+                String email = mEmail.getText().toString();
+                String password= mPassword.getText().toString();
 
                 registerUser(display_Name,email,password);
             }
         });
-
     }
 
     private void registerUser(String display_Name, String email, String password) {
-        mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-
-                            Log.d(TAG, "createUserWithEmail:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-
-                        } else {
-
-                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            Toast.makeText(RegisterActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-
-                        // ...
-                    }
-                });
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent mainIntent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this, "You got some error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
-
-
 }
